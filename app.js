a67f2f247e9c1998f1a2b27ac049e233a211b10d
Mohanarangan Muthukumar <mohan@extrasalt.org>
2015-11-07 17:48:07 +0530

    Added redirect to root path

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var oauth2Client = new OAuth2Client(cred.CLIENT_ID, cred.CLIENT_SECRET, "http://
 mongoose.connect('mongodb://localhost/test');
 
 
-var UserSchema = new mongoose.Schema({
+var UserSchema= mongoose.Schema({
   email: String,
   freetimestart: Number,
   freetimeend: Number,
@@ -41,30 +41,40 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
-app.get("/", function(req, res){
-var url = oauth2Client.generateAuthUrl({
-  access_type: 'offline',
-  scope: ['https://www.googleapis.com/auth/plus.me',
-          'https://www.googleapis.com/auth/userinfo.email',
-          'https://www.googleapis.com/auth/userinfo.profile']
-});
+var User = mongoose.model("User", UserSchema);
 
-res.send(url);
+app.get("/", function(req, res){
+  if(!req.session.email){
+    var url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: ['https://www.googleapis.com/auth/plus.me',
+              'https://www.googleapis.com/auth/userinfo.email',
+              'https://www.googleapis.com/auth/userinfo.profile']
+    });
+    res.redirect(url);
+  } else {
+    res.send("Hello, " + req.session.email);
+  }
 });
 
 
 app.get("/oauthcallback", function(req, res){
   var code = req.query.code;
 
-  console.log(code);
 
   oauth2Client.getToken(code, function(err, tokens){
     oauth2Client.setCredentials(tokens);
     authinfo.userinfo.get({fields: "email", auth:oauth2Client }, function(err, uinfo){
       req.session.email = uinfo.email;
       req.session.tokens = tokens;
-      console.log(err);
-      res.send(uinfo.email);
+
+      var user = new User({email:uinfo.email});
+      user.save(function(err,user) {
+        if(err){
+          console.log(err);
+        }
+      });
+      res.redirect("/");
     })
   });
 
