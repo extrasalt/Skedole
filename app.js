12481ee715a7e707bca505a66d3c6e6606d476b7
Mohanarangan Muthukumar <mohan@extrasalt.org>
2015-11-07 12:34:02 +0530

    Added Google OAuth

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var google = require('googleapis');
 var mongoose = require('mongoose');
+var cred = require('./credentials.js');
+
+
 var app = express();
+var OAuth2Client = google.auth.OAuth2;
+
+var authinfo = google.oauth2('v2');
+
+var oauth2Client = new OAuth2Client(cred.CLIENT_ID, cred.CLIENT_SECRET, "http://localhost:8080/oauthcallback")
 
 
 mongoose.connect('mongodb://localhost/test');
@@ -19,15 +27,35 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
+app.get("/", function(req, res){
+var url = oauth2Client.generateAuthUrl({
+  access_type: 'offline',
+  scope: ['https://www.googleapis.com/auth/plus.me',
+          'https://www.googleapis.com/auth/userinfo.email',
+          'https://www.googleapis.com/auth/userinfo.profile']
+});
 
-
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+res.send(url);
 });
 
 
-var port = 8080;
-app.listen(port);
+app.get("/oauthcallback", function(req, res){
+  var code = req.query.code;
+
+  console.log(code);
+
+  oauth2Client.getToken(code, function(err, tokens){
+    oauth2Client.setCredentials(tokens);
+    authinfo.userinfo.get({fields: "email", auth:oauth2Client }, function(err, uinfo){
+      console.log(err);
+      res.send(uinfo.email);
+    })
+  });
+
+
+});
+
+
 
 app.get("/schedule", function(req, res){
   res.send("GET schedule");
@@ -38,7 +66,8 @@ app.post("/schedule", function(req, res){
 });
 
 
-
+var port = 8080;
+app.listen(port);
 
 
 console.log('Express server started on port %s', port);
